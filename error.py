2de8e4d2a10d8e17a6cf44301d6f6df6fb4a7f1d
Jahnvi Patel <jpate201@uic.edu>
2019-03-07 14:28:31 -0600

    Clean up

--- error.py
+++ error.py
@@ -1,4 +1,13 @@
 import sys
+def instructions():
+    print ("-----------------------------------")
+    print ("Welcome to a python reader")
+    print ("Press 1 to read the inputted text.")
+    print ("Press 2 to read a .txt file")
+    print ("Press 3 to read a .pdf file")
+    print ("Press 4 to read a .png ")
+    print ("Press x to exit the program")
+    print ("-----------------------------------")
 
 def error1():
     print ("\nERROR: Sorry invalid input. Please try again...")
--- runall.py
+++ runall.py
@@ -1,16 +1,6 @@
 from readers import *
-
-#print errors
 from error import *  
 
-print ("-----------------------------------")
-print ("Welcome to a python reader")
-print ("Press 1 to read the inputted text.")
-print ("Press 2 to read a .txt file")
-print ("Press 3 to read a .pdf file")
-print ("Press 4 to read a .png ")
-print ("Press x to exit the program")
-print ("-----------------------------------")
 
 inputtedText = input("Please enter a number: ")
 lan = 'en'
@@ -30,6 +20,7 @@ elif (inputtedText == '3'):
 #.png Reader
 elif(inputtedText == '4'):
     pngReader()
+
 elif (inputtedText == 'x'):
     print ("Thank you for using Reader. Now exiting...")
     sys.exit(0)
